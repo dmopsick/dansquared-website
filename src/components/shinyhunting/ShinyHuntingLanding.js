17952b0fae874c86627afefa9051053a0b50974d
Dan Mopsick <danmopsick@gmail.com>
2024-08-02 17:39:08 -0400

    Adding the landing page with the first card... got it working

--- src/components/shinyhunting/ShinyHuntingLanding.js
+++ src/components/shinyhunting/ShinyHuntingLanding.js
@@ -1,4 +1,8 @@
 
+import Card from '@mui/material/Card';
+import {CardActionArea, CardContent, CardMedia, Typography} from '@mui/material';
+import DynamaxAdventurePic from '../../images/dynamax-adventures-card.png';
+
 export default function ShinyHuntingLanding() {
   return (
     <div className="pageContainer light-blue-background">
@@ -14,6 +18,25 @@ export default function ShinyHuntingLanding() {
         {/* Make card elements here to link to different calculators */}
 
         {/* Dynamax Adventure Calculator */}
+        <a href="/shinyhunting/dynamax-adventures">
+          <Card className='card'>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image={DynamaxAdventurePic}
+                alt="Dynamax Adventures in Pokémon Sword and Shield" />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Dynamax Adventures
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Catch 37 shiny Legendary Pokémon and 10 Shiny Ultra Beasts.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </a>
 
         {/* Base hunting calculator pre gen 6 (1/4096) */}
 
